c832db659af96bac5bda9eab59787991557adca6
LogeshM05 <112538088+LogeshM05@users.noreply.github.com>
2023-08-08 23:05:10 +0530

    Update activitylifecycle.py

--- activitylifecycle.py
+++ activitylifecycle.py
@@ -3,39 +3,39 @@ from java import jclass as autoclass
 Activity = autoclass('android.app.Activity')
 Bundle = autoclass('android.os.Bundle')
 
-# class PythonLifecyclePythonWrapper:
+class PythonLifecyclePythonWrapper:
 
-    @staticmethod
+    # @staticmethod
     def onActivityCreated(activity, savedInstanceState):
         # Called when an activity is created
         print("onActivityCreated:", activity)
 
-    @staticmethod
+    # @staticmethod
     def onActivityStarted(activity):
         # Called when an activity is started
         print("onActivityStarted:", activity)
 
-    @staticmethod
+    # @staticmethod
     def onActivityResumed(activity):
         # Called when an activity is resumed
         print("onActivityResumed:", activity)
 
-    @staticmethod
+    # @staticmethod
     def onActivityPaused(activity):
         # Called when an activity is paused
         print("onActivityPaused:", activity)
 
-    @staticmethod
+    # @staticmethod
     def onActivityStopped(activity):
         # Called when an activity is stopped
         print("onActivityStopped:", activity)
 
-    @staticmethod
+    # @staticmethod
     def onActivitySaveInstanceState(activity, outState):
         # Called when an activity's state is saved
         print("onActivitySaveInstanceState:", activity)
 
-    @staticmethod
+    # @staticmethod
     def onActivityDestroyed(activity):
         # Called when an activity is destroyed
         print("onActivityDestroyed:", activity)
