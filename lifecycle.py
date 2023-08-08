587b6bdd9f2fde0c68764b5900f2022206eab8b0
LogeshM05 <112538088+LogeshM05@users.noreply.github.com>
2023-08-08 11:49:33 +0530

    Add files via upload

---
+++ lifecycle.py
@@ -0,0 +1,29 @@
+from jnius import autoclass
+
+class AndroidInteraction:
+
+    def __init__(self, context):
+        self.context = context
+        self.AndroidBridge = autoclass('com.example.myapp.AndroidBridge')
+
+    def get_device_name(self):
+        bridge = self.AndroidBridge(self.context)
+        return bridge.getDeviceName()
+
+# def on_create():
+#     print("Python: Activity onCreate callback triggered")
+
+# def on_start():
+#     print("Python: Activity onStart callback triggered")
+
+# def on_resume():
+#     print("Python: Activity onResume callback triggered")
+
+# def on_pause():
+#     print("Python: Activity onPause callback triggered")
+
+# def on_stop():
+#     print("Python: Activity onStop callback triggered")
+
+# def on_destroy():
+#     print("Python: Activity onDestroy callback triggered")
