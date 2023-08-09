1d68fa4ce0b1676b68c892c03b089486f39ddab3
LogeshM05 <112538088+LogeshM05@users.noreply.github.com>
2023-08-09 12:38:08 +0530

    Update activitylifecycle.py

--- activitylifecycle.py
+++ activitylifecycle.py
@@ -1,25 +1,16 @@
-# lifecycle_listener.py
-from jnius import autoclass, PythonJavaClass, java_method
+# my_script.py
 
-Activity = autoclass('android.app.Activity')
-LifecycleObserver = autoclass('androidx.lifecycle.LifecycleObserver')
-Lifecycle = autoclass('androidx.lifecycle.Lifecycle')
-OnLifecycleEvent = autoclass('androidx.lifecycle.OnLifecycleEvent')
+from jnius import autoclass
 
-class MyLifecycleObserver(PythonJavaClass, LifecycleObserver):
-    __javainterfaces__ = ['androidx/lifecycle/LifecycleObserver']
+# Get the Java classes
+PythonActivity = autoclass('org.kivy.android.PythonActivity')
 
-    def __init__(self, callback):
-        super().__init__()
-        self.callback = callback
-
-    @java_method('()V')
-    def on_create(self):
-        self.callback('on_create')
-
-# Add other lifecycle event methods here
-
-def add_lifecycle_observer(activity, callback):
-    lifecycle = activity.getLifecycle()
-    observer = MyLifecycleObserver(callback)
-    lifecycle.addObserver(observer)
+def main():
+    # Get the Context of the current Android application
+    activity = PythonActivity.mActivity
+    context = activity.getApplicationContext()
+    
+    # Get the package name of the Context
+    package_name = context.getPackageName()
+    print("Package Name:", package_name)
+    
