407aed04524a847df64435ccb6f54169f85d389d
LogeshM05 <112538088+LogeshM05@users.noreply.github.com>
2023-08-09 10:36:37 +0530

    Update activitylifecycle.py

--- activitylifecycle.py
+++ activitylifecycle.py
@@ -1,50 +1,26 @@
-from java import jclass as autoclass
+import android
 
-# Java classes
-Context = autoclass('android.content.Context')
-Application = autoclass('android.app.Application')
-ActivityLifecycleCallbacks = autoclass('android.app.Application$ActivityLifecycleCallbacks')
+class MyLifecycleCallbacks:
+    def __init__(self, application):
+        self.application = application
 
-class LifecycleManager:
-    def __init__(self, context):
-        self.context = context
-        self.activity_lifecycle_callbacks = None
+    def onCreate(self):
+        android.makeToast("App: onCreate called")
 
-    def register_activity_callbacks(self):
-        try:
-            if self.activity_lifecycle_callbacks is None:
-                app = self.context.getApplicationContext()
-                self.activity_lifecycle_callbacks = ActivityLifecycleCallbacks()
-                app.registerActivityLifecycleCallbacks(self.activity_lifecycle_callbacks)
-        except Exception as e:
-            # Handle exceptions appropriately
-            pass
-            
-        def onActivityCreated(self, activity, savedInstanceState):
-            print("onActivityCreated called")
+    def onStart(self):
+        android.makeToast("App: onStart called")
 
-        def onActivityStarted(self, activity):
-            print("onActivityStarted called")
+    def onResume(self):
+        android.makeToast("App: onResume called")
 
-        def onActivityResumed(self, activity):
-            print("onActivityResumed called")
+    def onPause(self):
+        android.makeToast("App: onPause called")
 
-        def onActivityPaused(self, activity):
-            print("onActivityPaused called")
+    def onStop(self):
+        android.makeToast("App: onStop called")
 
-        def onActivityStopped(self, activity):
-            print("onActivityStopped called")
+    def onDestroy(self):
+        android.makeToast("App: onDestroy called")
 
-        def onActivitySaveInstanceState(self, activity, outState):
-            print("onActivitySaveInstanceState called")
-
-        def onActivityDestroyed(self, activity):
-            print("onActivityDestroyed called")
-
-
-
-
-# Instantiate the LifecycleManager and register callbacks
-context = autoclass('com.chaquo.python.Python').getPlatform().getApplication()
-lifecycle_manager = LifecycleManager(context)
-lifecycle_manager.register_activity_callbacks()
+    def registerLifecycleCallbacks(self):
+        self.application.registerActivityLifecycleCallbacks(self)
