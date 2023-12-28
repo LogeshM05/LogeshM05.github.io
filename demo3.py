bba7e77614c7eeb539c0cc8be67cb84ba1747de1
LogeshM05 <112538088+LogeshM05@users.noreply.github.com>
2023-12-28 17:17:37 +0530

    Update demo3.py

--- demo3.py
+++ demo3.py
@@ -1,6 +1,15 @@
+from com.example.pythondemo import DemoActivity
+
+# Initialize the Python interpreter
+AndroidPlatform.start_dir = None
+
 def hello_world():
     return 'Hello from Flask!'
 
 # @app.route('/location')
 def location():
     return 'This is your location!'
+
+def call_java_function():
+    result = DemoActivity.fromPython()
+    print(result)
