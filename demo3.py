7a476785b7ef6fd2bf59357c1127ccee9ac0ff1a
LogeshM05 <112538088+LogeshM05@users.noreply.github.com>
2023-12-28 18:02:31 +0530

    Update demo3.py

--- demo3.py
+++ demo3.py
@@ -1,11 +1,11 @@
-# from chaquopy import chaquopy_java_init
+from chaquopy import chaquopy_java_init
+chaquopy_java_init()
 from com.example.pythondemo import DemoActivity
 from android.app import ActivityLifecycleCallbacks, Application
 # import threading
 # import time
 
 # current_lifecycle_status = None
-# chaquopy_java_init()
 
 class MyActivityLifecycleCallbacks(ActivityLifecycleCallbacks):
     def onActivityResumed(self, activity):
