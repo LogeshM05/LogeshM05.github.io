03125c2b2f14b45aaabb53ed4ff05281129bbfd5
LogeshM05 <112538088+LogeshM05@users.noreply.github.com>
2023-12-28 17:40:34 +0530

    Update demo3.py

--- demo3.py
+++ demo3.py
@@ -1,7 +1,36 @@
 from chaquopy import chaquopy_java_init
 from com.example.pythondemo import DemoActivity
+from android.app import ActivityLifecycleCallbacks, Application
+import threading
+import time
 
 chaquopy_java_init()
+
+current_lifecycle_status = None
+
+class MyActivityLifecycleCallbacks(ActivityLifecycleCallbacks):
+    def onActivityResumed(self, activity):
+        global current_lifecycle_status
+        current_lifecycle_status = "Resumed"
+
+    def onActivityPaused(self, activity):
+        global current_lifecycle_status
+        current_lifecycle_status = "Paused"
+
+# Register the ActivityLifecycleCallbacks
+application = Application.getApplication()
+callbacks = MyActivityLifecycleCallbacks()
+application.registerActivityLifecycleCallbacks(callbacks)
+
+# Define a function to periodically check the lifecycle status
+def check_lifecycle_status():
+    while True:
+        time.sleep(5)  # Sleep for 5 seconds
+        print("Current Activity Lifecycle Status:", current_lifecycle_status)
+
+# Start the thread to periodically check the lifecycle status
+thread = threading.Thread(target=check_lifecycle_status)
+thread.start()
 
 def hello_world():
     return 'Hello from Flask!'
@@ -13,3 +42,6 @@ def location():
 def call_java_function():
     result = DemoActivity.fromPython()
     return result
+
+def get_activity_lifecycle():
+    
