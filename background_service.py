efc5a521bce4c061962924b9fff1100ed3cbfe48
LogeshM05 <112538088+LogeshM05@users.noreply.github.com>
2023-12-28 18:15:20 +0530

    Create background_service.py

---
+++ background_service.py
@@ -0,0 +1,10 @@
+# background_service.py
+from android import Android
+import time
+
+android = Android()
+
+def run_background_service():
+    while True:
+        android.notification('Background Service', 'Running...')
+        time.sleep(10)  # Sleep for 10 seconds
